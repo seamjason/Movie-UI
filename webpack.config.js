e0251f0d2bdad4e6b5e2f980073d41a96de5fb87
Jason Sutton <jason.sutton@theseam.com>
2024-07-15 12:31:54 -0500

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+var path = require('path');
+
+module.exports = {
+    entry: './app.js',
+    output: {
+        path: path.join(__dirname, 'Built'),
+        filename: 'movie-ui.bundle.js'
+    }
+};
